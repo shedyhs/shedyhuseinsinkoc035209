a5a16ac841ba1fdf662021ea1f2a41228597e456
Shedy <shedyhs@gmail.com>
2026-01-31 20:01:25 -0400

    Add Flyway migrations to seed sample artists, albums and test user
    V6: Insert 4 artists (Serj Tankian, Mike Shinoda, Michel Telo, Guns N Roses)
    V7: Insert 14 albums across all artists
    V8: Link artists to albums via junction table
    V9: Insert admin test user (admin/admin123) with BCrypt password

---
+++ src/main/resources/db/migration/V9__insert_test_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, username, password, role) VALUES
+    ('c1111111-1111-1111-1111-111111111111', 'admin', '$2a$10$lb98h2xR4PZnpt82WceHS.8G4U.m9S6w/cbExf.ULnpCXqyWitPe2', 'ADMIN');
